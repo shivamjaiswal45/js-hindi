5488606990eb72035565f4fed42f77dfefa131b0
Shivam Jaiswal <shivammjaiswal45@gmail.com>
2025-09-16 20:21:44 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "shivam@google.com"
+var accountPassword = "476598"
+accountCity = "Kushinagar"
+// accountId = 2
+accountEmail = "sj@sj.com"
+accountPassword = "21212121"
+accountCity = "Kasia"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
+  
